d09f914e5871f6b22108faabb9b95652e30a4f7e
shmakov73 <shmakov73@gmail.com>
2022-04-29 15:52:06 +0400

    сохранение перед исправлением переменных, применением StringBuilder для сохранения текста

--- src/com/cryptoAnalyzer/Analyzer.java
+++ src/com/cryptoAnalyzer/Analyzer.java
@@ -1,4 +1,44 @@
 package com.cryptoAnalyzer;
 
 public class Analyzer {
+    static String stringAlphaBet = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя.,”:-!? ";
+    public static char[] alphaBet = stringAlphaBet.toCharArray();
+
+    public static int letterCount(StringBuilder text, char symbol){
+        int c = 0;
+        char[] ch = text.toString().toCharArray();
+        for (int i = 0; i < text.length(); i++) {
+            if (ch[i] == symbol) {
+                c++;
+            }
+        }
+        return c;
+    }
+
+    public static String encrypt(String stringMessage, int cryptoKey){
+        char[] message = stringMessage.toCharArray();
+        char[] result = new char[message.length];
+        for (int i = 0; i < message.length; i++) {
+            for (int j = 0; j < alphaBet.length; j++) {
+                if (message[i] == alphaBet[j]){
+                    result[i] = alphaBet[(alphaBet.length + j + cryptoKey) % alphaBet.length];
+                }
+            }
+        }
+        return String.valueOf(result);
+
+    }
+
+    public static String decrypt(String encryptedText, int cryptoKey){
+        char[] text = encryptedText.toCharArray();
+        char[] result = new char[text.length];
+        for (int i = 0; i < text.length; i++) {
+            for (int j = 0; j < alphaBet.length; j++) {
+                if (text[i] == alphaBet[j]){
+                    result[i] = alphaBet[(alphaBet.length + j - cryptoKey) % alphaBet.length];
+                }
+            }
+        }
+        return String.valueOf(result);
+    }
 }
--- src/com/cryptoAnalyzer/Main.java
+++ src/com/cryptoAnalyzer/Main.java
@@ -3,11 +3,9 @@ package com.cryptoAnalyzer;
 import java.io.*;
 
 public class Main {
-    static String stringAlphaBet = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя.,”:-!? ";
-    public static char[] alphaBet = stringAlphaBet.toCharArray();
 
     public static void main(String[] args) throws IOException {
-        int key = 0;
+        int key = 5;
 //        Scanner console = new Scanner(System.in);
 //        String line1 = console.nextLine();
         String inputFile = "c:\\test\\tolstoy.txt";
@@ -20,63 +18,38 @@ public class Main {
              BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))){
             while (reader.ready()){
                 String line = reader.readLine();
-                //System.out.println(line);
-                String encryptLine = encrypt(line, key);
+                String encryptLine =Analyzer.encrypt(line, key);
                 writer.write(encryptLine + "\n");
                 text.append(encryptLine);
                 writer.flush();
             }
-            int c = 0;
-            char[] ch = text.toString().toCharArray();
-            for (int i = 0; i < text.length(); i++) {
-                if (ch[i] == 'ъ'){
-                    c++;
-                }
-            }
-            System.out.println(c);
+
+            int s = Analyzer.letterCount(text, 'а');
+            int s1 = Analyzer.letterCount(text, 'с');
 
         }
-        System.out.println(text.length());
-
-//        try (FileReader in = new FileReader(outputFile);
-//             BufferedReader reader = new BufferedReader(in);
-//             BufferedWriter writer = new BufferedWriter(new FileWriter(decryptedFile))){
-//            while (reader.ready()){
-//                String line = reader.readLine();
-//                System.out.println(line);
-//                String decryptLine = decrypt(line, key);
-//                writer.write(decryptLine + "\n");
-//                writer.flush();
-//            }
-//        }
-// *************************** зашифровка текста сдвигом по алфавиту на значение key ***************************
-    }
-    public static String encrypt(String stringMessage, int cryptoKey){
-        char[] message = stringMessage.toCharArray();
-        char[] result = new char[message.length];
-        for (int i = 0; i < message.length; i++) {
-            for (int j = 0; j < alphaBet.length; j++) {
-                if (message[i] == alphaBet[j]){
-                    result[i] = alphaBet[(alphaBet.length + j + cryptoKey) % alphaBet.length];
-                }
+        try (FileReader in = new FileReader(outputFile);
+             BufferedReader reader = new BufferedReader(in);
+             BufferedWriter writer = new BufferedWriter(new FileWriter(decryptedFile))){
+            while (reader.ready()){
+                String line = reader.readLine();
+                System.out.println(line);
+                String decryptLine = Analyzer.decrypt(line, key);
+                writer.write(decryptLine + "\n");
+                writer.flush();
             }
         }
-        return String.valueOf(result);
-
-    }
-
-    //********************************* расшифровка текста сдвигом по алфавиту на значение key *************************
-
-    public static String decrypt(String encryptedText, int cryptoKey){
-        char[] text = encryptedText.toCharArray();
-        char[] result = new char[text.length];
-        for (int i = 0; i < text.length; i++) {
-            for (int j = 0; j < alphaBet.length; j++) {
-                if (text[i] == alphaBet[j]){
-                    result[i] = alphaBet[(alphaBet.length + j - cryptoKey) % alphaBet.length];
-                }
+        int aLength = Analyzer.alphaBet.length;
+        for (int i = 0; i < aLength; i++) {
+            String s = Analyzer.decrypt(outputFile, i);
+            if (Analyzer.letterCount(text, 'о') > Analyzer.letterCount(text, 'е') &&
+                    Analyzer.letterCount(text, 'е') > Analyzer.letterCount(text, 'и') &&
+                    Analyzer.letterCount(text, 'и') > Analyzer.letterCount(text, 'с') &&
+                    Analyzer.letterCount(text, 'с') > Analyzer.letterCount(text, 'у') &&
+                    Analyzer.letterCount(text, 'у') > Analyzer.letterCount(text, 'ф')){
+                System.out.println(i);
+                break;
             }
         }
-        return String.valueOf(result);
     }
 }
